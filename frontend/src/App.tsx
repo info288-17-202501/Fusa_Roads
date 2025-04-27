5a4149fe4fe094c2fa65856934b47b1bbbc58c40
SebaMonte07 <seb.monte17@gmail.com>
2025-04-26 23:38:55 -0400

    feat: crear pagina de secciones calles

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Home from './pages/Home'; 
+import SeccioneCalles from './pages/SeccionesCalles'
 
 
 const App: React.FC = () => {
@@ -9,6 +10,8 @@ const App: React.FC = () => {
       <Routes>
         <Route path="/" element={<Navigate to="/home" />} /> 
         <Route path="/home" element={<Home/>} /> 
+        <Route path="/secciones-calles" element={<SeccioneCalles/>} /> 
+
 
       </Routes>
     </Router>
