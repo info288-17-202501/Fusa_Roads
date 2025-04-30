3d5c41af22e0088aec1fcd63212da519d3db78ab
SebaMonte07 <seb.monte17@gmail.com>
2025-04-30 11:16:26 -0400

    feat: Botones del nav se quedan activados

--- frontend/src/components/NavigationBar.tsx
+++ frontend/src/components/NavigationBar.tsx
@@ -1,25 +1,44 @@
-import { Container, Nav, Navbar } from "react-bootstrap";
+import { Container, Nav, Navbar, Button } from "react-bootstrap";
 import FusaLogo from "../assets/fusa_logo.png";
+import { useLocation } from "react-router-dom";
+
+const NavButtons = [
+	{ ruta: "/home", etiqueta: "Home", disabled: false },
+	{ ruta: "/videos", etiqueta: "Videos", disabled: false },
+	{ ruta: "/secciones-calles", etiqueta: "Secciones Calles", disabled: false },
+	{ ruta: "/modelos-ia", etiqueta: "Modelos IA", disabled: true },
+	{ ruta: "/proyectos-ia", etiqueta: "PIA", disabled: true },
+	{ ruta: "/monitor-procesos", etiqueta: "Monitor de Procesos", disabled: true },
+	{ ruta: "/proyecto-mapa-de-ruido", etiqueta: "PMR", disabled: true },
+	{ ruta: "/visor-mapas-de-ruido", etiqueta: "Visor de Mapas", disabled: true }
+];
+  
 
 function NavigationBar() {
+	const location = useLocation();
+	const currentPath = location.pathname;
+
   	return (
     	<Navbar expand="lg" style={{ backgroundColor: "#eee" }} data-bs-theme="light">
-      		<Container>
+      		<Container> {/* className="d-flex align-items-end" para que los botones del nav se peguen abajo */}
        			<Navbar.Brand href="/home">
-          			<img src={FusaLogo} height="50" alt="FusaLogo.png" />
+          			<img src={FusaLogo} height="55" alt="FusaLogo.png" />
         		</Navbar.Brand>
         		<Navbar.Toggle aria-controls="navbar-responsive" />
         		<Navbar.Collapse id="navbar-responsive">
-					<Nav className="me-auto">
-						<Nav.Link href="/home">Home</Nav.Link>
-						<Nav.Link href="/videos">Videos</Nav.Link>
-						<Nav.Link href="/secciones-calles">Secciones Calles</Nav.Link>
-						<Nav.Link disabled href="/modelos-ia">Modelos IA</Nav.Link>
-						<Nav.Link disabled href="/proyectos-ia">PIA</Nav.Link>
-						<Nav.Link disabled href="/monitor-procesos">Monitor de Procesos</Nav.Link>
-						<Nav.Link disabled href="/proyecto-mapa-de-ruido">PMR</Nav.Link>
-						<Nav.Link disabled href="/visor-mapas-de-ruido">Visor de Mapas</Nav.Link>
+					<Nav className="me-auto"> {/* mx-auto para que los botones del nav se centren */}
+						{NavButtons.map((button) => (
+							<Nav.Link
+								href={button.ruta}
+								disabled={button.disabled}
+								className="rounded-5 me-auto"
+								style={{backgroundColor: currentPath === button.ruta ? "#ddd" : ""}}
+							>
+								{button.etiqueta}
+							</Nav.Link>
+						))}
 					</Nav>
+					<Button>Log in</Button>
         		</Navbar.Collapse>
       		</Container>
     	</Navbar>
