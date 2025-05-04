549d8674c100b0bf230738a527a0c05648308ec3
SebaMonte07 <seb.monte17@gmail.com>
2025-05-04 16:16:47 -0400

    feature: crear modal de agregar nueva seccion de calle de manera individual

--- frontend/src/SeccionesCalles/components/ModalNuevaSeccionCalle.tsx
+++ frontend/src/SeccionesCalles/components/ModalNuevaSeccionCalle.tsx
@@ -1,16 +1,59 @@
-import { Modal, Button } from "react-bootstrap";
+import { Modal, Button, Row, Form } from "react-bootstrap";
+import { useState, useEffect } from 'react'
+
+interface RegionData{
+    region: string;
+    comunas: string[];
+}
 
 interface Props {
     show: boolean;
     onClose: () => void;
-    onSubmit?: () => void;
+    data: RegionData[];
 }
 
-function ModalNuevaSeccionCalle ({show, onClose, onSubmit}: Props){
+function ModalNuevaSeccionCalle ({show, onClose, data}: Props){
+    const [pais, setPais] = useState('');
+    const [region, setRegion] = useState('');
+    const [comuna, setComuna] = useState('');
 
+    const [regiones, setRegiones] = useState<RegionData[]>([]);
+    const [comunas, setComunas] = useState<string[]>([]);
+
+    useEffect(()=> {
+        if(pais == 'Chile'){
+            setRegiones(data)
+        } else {
+            setRegiones([]);
+            setRegion('');
+            setComuna('');
+        }
+    }, [pais, data]);
+
+    useEffect(() => {
+        const selectedRegion = regiones.find((r) => r.region === region);
+        if(selectedRegion){
+            setComunas(selectedRegion.comunas);
+        } else {
+            setComunas([]);
+        }
+    }, [region, regiones]);
+
+    const handleCancelar = () => {
+        setPais('');
+        setRegion('');
+        setComuna('');
+        // Dejar archivo vació
+        onClose();
+    }
+
+    const handleGuardar = () => {
+        console.log({pais, region, comuna});
+        onClose();
+    }
 
     return (
-        <Modal show={show} onHide={onClose} centered>
+        <Modal show={show} onHide={handleCancelar} centered scrollable>
             <Modal.Header closeButton>
                 <Modal.Title>
                     Nueva Sección de Calle
@@ -18,13 +61,69 @@ function ModalNuevaSeccionCalle ({show, onClose, onSubmit}: Props){
             </Modal.Header>
             
             <Modal.Body>
-                Aqui van los forms
+                <Row className='justify-content-center mb-3'>
+                    <Form.Group className="w-75">    
+                        <Form.Label>País:</Form.Label>
+                        <Form.Select value={pais} onChange={(e) => setPais(e.target.value)}>
+                            <option value={''}>Seleccione País</option>
+                            <option>Chile</option>
+                        </Form.Select>
+                    </Form.Group>
+                </Row>
+                <Row className='justify-content-center mb-3'>
+                    <Form.Group className="w-75">
+                        <Form.Label>Región:</Form.Label>
+                        <Form.Select disabled={pais ==''} value={region} onChange={(e) => setRegion(e.target.value)}>
+                            <option value={''}>Seleccione Región</option>
+                            {regiones.map((r) => (
+                                <option key={r.region}>{r.region}</option>
+                            ))}
+                        </Form.Select>
+                    </Form.Group>
+                </Row>
+                <Row className='justify-content-center mb-3'>
+                    <Form.Group className="w-75">
+                        <Form.Label>Comuna:</Form.Label>
+                        <Form.Select disabled={region == ''} value={comuna} onChange={(e) => setComuna(e.target.value)}>
+                            <option value={''}>Seleccione Comuna</option>
+                            {comunas.map((c) => (
+                                <option key={c}>{c}</option>
+                            ))}
+                        </Form.Select>
+                    </Form.Group>
+                </Row>
+                <Row className='justify-content-center mb-3'>
+                    <Form.Group className="w-75">
+                        <Form.Label>Nombre Calle:</Form.Label>
+                        <Form.Control/>
+                    </Form.Group>
+                </Row>
+                <Row className='justify-content-center mb-3'>
+                    <Form.Group className="w-75">    
+                        <Form.Label>App:</Form.Label>
+                        <Form.Select>
+                            <option value={''}>Seleccione Modelo de IA</option>
+                            <option>CadnaA</option>
+                            <option>NoiseModelling</option>
+                        </Form.Select>
+                    </Form.Group>
+                </Row>
+                <Row className='justify-content-center mb-3'>
+                    <Form.Group className="w-75">    
+                        <Form.Label>Tipo de Vía:</Form.Label>
+                        <Form.Select>
+                            <option value={''}>Seleccione el tipo de vía</option>
+                            <option>Troncal</option>
+                            <option>Federal</option>
+                        </Form.Select>
+                    </Form.Group>
+                </Row>
                 
             </Modal.Body>
 
             <Modal.Footer>
-                <Button variant='secondary' onClick={onClose}>Cancelar</Button>
-                <Button variant='primary' onClick={onSubmit}>Guardar</Button>
+                <Button variant='secondary' onClick={handleCancelar}>Cancelar</Button>
+                <Button variant='primary' onClick={handleGuardar}>Guardar</Button>
             </Modal.Footer>
         </Modal>
     )
