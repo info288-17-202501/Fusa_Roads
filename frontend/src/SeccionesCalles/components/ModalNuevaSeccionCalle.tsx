04f6e1a95ce7749252e5a12cfa8521f4f0ae7417
SebaMonte07 <seb.monte17@gmail.com>
2025-05-04 17:39:01 -0400

    refactor: crear componente para los formularios de Nueva Seccion de Calle y Carga masiva de Secciones de calle

--- frontend/src/SeccionesCalles/components/ModalNuevaSeccionCalle.tsx
+++ frontend/src/SeccionesCalles/components/ModalNuevaSeccionCalle.tsx
@@ -1,5 +1,6 @@
-import { Modal, Button, Row, Form } from "react-bootstrap";
-import { useState, useEffect } from 'react'
+import { Modal, Button, Form } from "react-bootstrap";
+import { useState, useEffect, FormEvent } from 'react'
+import FormRow from "./FormRow";
 
 interface RegionData{
     region: string;
@@ -13,9 +14,14 @@ interface Props {
 }
 
 function ModalNuevaSeccionCalle ({show, onClose, data}: Props){
+    const [validated, setValidated] = useState(false);
+
     const [pais, setPais] = useState('');
     const [region, setRegion] = useState('');
     const [comuna, setComuna] = useState('');
+    const [nombreCalle, setNombreCalle] = useState('');
+    const [app, setApp] = useState('');
+    const [tipoVia, setTipoVia] = useState('');
 
     const [regiones, setRegiones] = useState<RegionData[]>([]);
     const [comunas, setComunas] = useState<string[]>([]);
@@ -40,91 +46,100 @@ function ModalNuevaSeccionCalle ({show, onClose, data}: Props){
     }, [region, regiones]);
 
     const handleCancelar = () => {
+        // Limpiar todos los campos
         setPais('');
         setRegion('');
         setComuna('');
-        // Dejar archivo vació
+        setNombreCalle('');
+        setApp('');
+        setTipoVia('');
+        setValidated(false);
         onClose();
     }
 
-    const handleGuardar = () => {
-        console.log({pais, region, comuna});
+    const handleGuardarYLimpiar = () => {
+        console.log({pais, region, comuna, nombreCalle, app, tipoVia});
+        // Aqui se deberia manejar la logica para mandar a la BD (podemos unir esta funcion con la de handleSubmit)
+        // Limpiar todos los campos
+        setPais('');
+        setRegion('');
+        setComuna('');
+        setNombreCalle('');
+        setApp('');
+        setTipoVia('');
+        setValidated(false);
         onClose();
     }
+
+    const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        const form = event.currentTarget;
+        if (!form.checkValidity()) { //Si no es válido, entra aquí
+            event.stopPropagation();
+            setValidated(true);
+            return;
+        }
+    
+        setValidated(true);
+        handleGuardarYLimpiar();
+    };
 
     return (
-        <Modal show={show} onHide={handleCancelar} centered scrollable>
+        <Modal show={show} onHide={handleCancelar} centered>
             <Modal.Header closeButton>
                 <Modal.Title>
                     Nueva Sección de Calle
                 </Modal.Title>
             </Modal.Header>
             
-            <Modal.Body>
-                <Row className='justify-content-center mb-3'>
-                    <Form.Group className="w-75">    
-                        <Form.Label>País:</Form.Label>
-                        <Form.Select value={pais} onChange={(e) => setPais(e.target.value)}>
+            <Form noValidate validated={validated} onSubmit={handleSubmit}>
+                <Modal.Body>
+                    <FormRow label="País:" showFeedback>
+                        <Form.Select required value={pais} onChange={(e) => setPais(e.target.value)}>
                             <option value={''}>Seleccione País</option>
                             <option>Chile</option>
                         </Form.Select>
-                    </Form.Group>
-                </Row>
-                <Row className='justify-content-center mb-3'>
-                    <Form.Group className="w-75">
-                        <Form.Label>Región:</Form.Label>
-                        <Form.Select disabled={pais ==''} value={region} onChange={(e) => setRegion(e.target.value)}>
+                    </FormRow>
+                    <FormRow label="Región:" showFeedback>
+                        <Form.Select required disabled={pais ==''} value={region} onChange={(e) => setRegion(e.target.value)}>
                             <option value={''}>Seleccione Región</option>
                             {regiones.map((r) => (
                                 <option key={r.region}>{r.region}</option>
                             ))}
                         </Form.Select>
-                    </Form.Group>
-                </Row>
-                <Row className='justify-content-center mb-3'>
-                    <Form.Group className="w-75">
-                        <Form.Label>Comuna:</Form.Label>
-                        <Form.Select disabled={region == ''} value={comuna} onChange={(e) => setComuna(e.target.value)}>
+                    </FormRow>
+                    <FormRow label="Comuna:" showFeedback>
+                        <Form.Select required disabled={region == ''} value={comuna} onChange={(e) => setComuna(e.target.value)}>
                             <option value={''}>Seleccione Comuna</option>
                             {comunas.map((c) => (
                                 <option key={c}>{c}</option>
                             ))}
                         </Form.Select>
-                    </Form.Group>
-                </Row>
-                <Row className='justify-content-center mb-3'>
-                    <Form.Group className="w-75">
-                        <Form.Label>Nombre Calle:</Form.Label>
-                        <Form.Control/>
-                    </Form.Group>
-                </Row>
-                <Row className='justify-content-center mb-3'>
-                    <Form.Group className="w-75">    
-                        <Form.Label>App:</Form.Label>
-                        <Form.Select>
+                    </FormRow>
+                    <FormRow label="Nombre de la Calle" showFeedback>
+                        <Form.Control placeholder="Ingrese el nombre de la calle" required onChange={(e) => setNombreCalle(e.target.value)}/>
+                    </FormRow>
+                    <FormRow label="App:" showFeedback>
+                        <Form.Select required onChange={(e) => setApp(e.target.value)}>
                             <option value={''}>Seleccione Modelo de IA</option>
                             <option>CadnaA</option>
                             <option>NoiseModelling</option>
                         </Form.Select>
-                    </Form.Group>
-                </Row>
-                <Row className='justify-content-center mb-3'>
-                    <Form.Group className="w-75">    
-                        <Form.Label>Tipo de Vía:</Form.Label>
-                        <Form.Select>
+                    </FormRow>
+                    <FormRow label="Tipo de Vía:" showFeedback>
+                        <Form.Select required onChange={(e) => setTipoVia(e.target.value)}>
                             <option value={''}>Seleccione el tipo de vía</option>
                             <option>Troncal</option>
                             <option>Federal</option>
                         </Form.Select>
-                    </Form.Group>
-                </Row>
-                
-            </Modal.Body>
+                    </FormRow>
+                </Modal.Body>
 
-            <Modal.Footer>
-                <Button variant='secondary' onClick={handleCancelar}>Cancelar</Button>
-                <Button variant='primary' onClick={handleGuardar}>Guardar</Button>
-            </Modal.Footer>
+                <Modal.Footer>
+                    <Button variant='secondary' onClick={handleCancelar}>Cancelar</Button>
+                    <Button type="submit">Guardar</Button>
+                </Modal.Footer>
+            </Form>
         </Modal>
     )
 }
