506cda5da6417a68ac4acfbe6699337d9d164c1d
SebaMonte07 <seb.monte17@gmail.com>
2025-05-04 17:39:44 -0400

    feature: agregar manejo de archivos para Carga masiva de secciones de calle

--- frontend/src/SeccionesCalles/components/ModalCargaMasiva.tsx
+++ frontend/src/SeccionesCalles/components/ModalCargaMasiva.tsx
@@ -1,5 +1,6 @@
-import { Modal, Row, Form, Button } from "react-bootstrap";
-import { useState, useEffect } from 'react'
+import { Modal, Form, Button } from "react-bootstrap";
+import { useState, useEffect, FormEvent } from 'react'
+import FormRow from "./FormRow";
 
 interface RegionData{
     region: string;
@@ -13,9 +14,12 @@ interface Props {
 }
 
 function ModalCargaMasiva ({show, onClose, data}: Props){
+    const [validated, setValidated] = useState(false);
+
     const [pais, setPais] = useState('');
     const [region, setRegion] = useState('');
     const [comuna, setComuna] = useState('');
+    const [archivo, setArchivo] = useState<File | null>(null);
 
     const [regiones, setRegiones] = useState<RegionData[]>([]);
     const [comunas, setComunas] = useState<string[]>([]);
@@ -43,14 +47,34 @@ function ModalCargaMasiva ({show, onClose, data}: Props){
         setPais('');
         setRegion('');
         setComuna('');
-        // Dejar archivo vació
+        setArchivo(null);
+        setValidated(false);
         onClose();
     }
 
-    const handleGuardar = () => {
-        console.log({pais, region, comuna});
+    const handleGuardarYLimpiar = () => {
+        console.log({pais, region, comuna, archivo});
+        // Aqui se deberia manejar la logica para mandar a la BD (podemos unir esta funcion con la de handleSubmit)
+        // Limpiar todos los campos
+        setPais('');
+        setRegion('');
+        setComuna('');
+        setArchivo(null)
+        setValidated(false);
         onClose();
     }
+    const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        const form = event.currentTarget;
+        if (!form.checkValidity() || !archivo) { //Si no es válido o no hay archivo subido, entra aquí
+            event.stopPropagation();
+            setValidated(true);
+            return;
+        }
+    
+        setValidated(true);
+        handleGuardarYLimpiar();
+    };
 
 
     return (
@@ -60,51 +84,49 @@ function ModalCargaMasiva ({show, onClose, data}: Props){
                     Carga Masiva de Secciones de Calle
                 </Modal.Title>
             </Modal.Header>
-            
-            <Modal.Body>
-                <Row className='justify-content-center mb-3'>
-                    <Form.Group className="w-75">    
-                        <Form.Label>País:</Form.Label>
-                        <Form.Select value={pais} onChange={(e) => setPais(e.target.value)}>
+                
+            <Form noValidate validated={validated} onSubmit={handleSubmit}>
+                <Modal.Body>
+                    <FormRow label="País:" showFeedback>
+                        <Form.Select required value={pais} onChange={(e) => setPais(e.target.value)}>
                             <option value={''}>Seleccione País</option>
                             <option>Chile</option>
                         </Form.Select>
-                    </Form.Group>
-                </Row>
-                <Row className='justify-content-center mb-3'>
-                    <Form.Group className="w-75">
-                        <Form.Label>Región:</Form.Label>
-                        <Form.Select disabled={pais ==''} value={region} onChange={(e) => setRegion(e.target.value)}>
+                    </FormRow>
+                    <FormRow label="Región:" showFeedback>
+                        <Form.Select required disabled={pais ==''} value={region} onChange={(e) => setRegion(e.target.value)}>
                             <option value={''}>Seleccione Región</option>
                             {regiones.map((r) => (
                                 <option key={r.region}>{r.region}</option>
                             ))}
                         </Form.Select>
-                    </Form.Group>
-                </Row>
-                <Row className='justify-content-center mb-3'>
-                    <Form.Group className="w-75">
-                        <Form.Label>Comuna:</Form.Label>
-                        <Form.Select disabled={region == ''} value={comuna} onChange={(e) => setComuna(e.target.value)}>
+                    </FormRow>
+                    <FormRow label="Comuna:" showFeedback>
+                        <Form.Select required disabled={region == ''} value={comuna} onChange={(e) => setComuna(e.target.value)}>
                             <option value={''}>Seleccione Comuna</option>
                             {comunas.map((c) => (
                                 <option key={c}>{c}</option>
                             ))}
                         </Form.Select>
-                    </Form.Group>
-                </Row>
-                <Row className='justify-content-center mb-3'>
-                    <Form.Group className="w-75">
-                        <Form.Label>Subir archivo</Form.Label>
-                        <Form.Control type='file'/>
-                    </Form.Group>
-                </Row>
-            </Modal.Body>
+                    </FormRow>
+                    <FormRow label="Subir archivo:" showFeedback>
+                        <Form.Control
+                            required
+                            type='file'
+                            accept=".csv"
+                            onChange={(e) => {
+                                const target = e.target as HTMLInputElement;
+                                setArchivo(target.files?.[0] || null)
+                            }}
+                        />
+                    </FormRow>
+                </Modal.Body>
 
-            <Modal.Footer>
-                <Button variant='secondary' onClick={handleCancelar}>Cancelar</Button>
-                <Button variant='primary' onClick={handleGuardar}>Guardar</Button>
-            </Modal.Footer>
+                <Modal.Footer>
+                    <Button variant='secondary' onClick={handleCancelar}>Cancelar</Button>
+                    <Button type="submit">Guardar</Button>
+                </Modal.Footer>
+            </Form>
         </Modal>
     )
 }
