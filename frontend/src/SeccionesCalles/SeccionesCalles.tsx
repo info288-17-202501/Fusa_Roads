a8b99b311064dff7e472081ac78f3fa9e7537e01
SebaMonte07 <seb.monte17@gmail.com>
2025-05-04 21:21:54 -0400

    feat: agregar funcionalidad del botón de eliminar en la tabla de secciones calles

--- frontend/src/SeccionesCalles/SeccionesCalles.tsx
+++ frontend/src/SeccionesCalles/SeccionesCalles.tsx
@@ -1,10 +1,11 @@
 import Table from '../components/Table';
 import { Container } from 'react-bootstrap';
 import { columns } from './resources/columns'
-import { calles } from './resources/callesData';
+import { calles as callesData } from './resources/callesData'; //Cambiar esto por un GET a la base de datos
 import { useState } from 'react';
 import ModalCargaMasiva from './components/ModalCargaMasiva';
 import ModalNuevaSeccionCalle from './components/ModalNuevaSeccionCalle';
+import ModalConfirmacion from './components/ModalConfirmacion'
 import regiones_comunas from './resources/comunas-regiones.json'
 
 
@@ -12,12 +13,73 @@ function SeccionesCalles() {
     const [showNuevo, setShowNuevo] = useState(false);
     const [showCargaMasiva, setShowCargaMasiva] = useState(false);
 
+    const [calles, setCalles] = useState(callesData)
+    const [showConfirmarModal, setShowConfirmarModal] = useState(false);
+    const [message, setMessage] = useState<React.ReactNode>(null)
+    const [deleteId, setDeleteId] = useState<number | null>(null);
+
+    const handleAskDelete = (id: number, nombre: string) => {
+        setDeleteId(id);
+        setMessage( // Quedó así para poder poner letra en negrita
+            <> 
+                ¿Estás seguro que desear eliminar la sección de calle <strong>{nombre}</strong> con <strong>ID {id}</strong>?
+            </>
+        )
+        // setMessage(`¿Estás seguro que deseas eliminar la sección de calle "${nombre}" con ID ${id}?`)
+        setShowConfirmarModal(true);
+    }
+
+    const handleConfirmDelete = () => {
+        if(deleteId === null) return;
+
+        setCalles((prev) => prev.filter((c) => c.id !== deleteId))
+        
+        setShowConfirmarModal(false);
+        setDeleteId(null);
+    }
+
+    // Cuando tengamos el backend, cambiar la forma de obtener los datos por este useEffecto
+    // useEffect(() => {
+    //     const fetchCalles = async () => {
+    //         try {
+    //             const response = await fetch('http://tu-backend.com/api/calles'); // <- cambia esto
+    //             const data = await response.json();
+    //             setCalles(data);
+    //         } catch (error) {
+    //             console.error("Error al cargar calles:", error);
+    //         }
+    //     };
+    //     fetchCalles();
+    // }, []);
+
+    // Cuando tengamos el backend cambiar la funcion handleConfirmDelete:
+    // const handleConfirmDelete = async () => {
+    //     if (deleteId === null) return;
+    
+    //     try {
+    //         await fetch(`http://tu-backend.com/api/calles/${deleteId}`, {
+    //             method: 'DELETE',
+    //         });
+    
+    //         setCalles((prev) => prev.filter((calle) => calle.id !== deleteId));
+    //     } catch (error) {
+    //         console.error("Error al eliminar calle:", error);
+    //         // Podrías mostrar un toast o alerta al usuario
+    //     } finally {
+    //         setShowConfirmarModal(false);
+    //         setDeleteId(null);
+    //     }
+    // };
+    
+    
+
+
     return (
         <>
             <Container className="w-75 my-5">
                 <h1 className="d-flex justify-content-center mb-4">Secciones Calles</h1>
                 <Table
-                    columns={columns}
+                    columns={columns(handleAskDelete)}
                     data={calles}
                     showNewButton={true}
                     onClickNewButton={() => setShowNuevo(true)}
@@ -36,6 +98,13 @@ function SeccionesCalles() {
                 show={showNuevo}
                 onClose={() => setShowNuevo(false)}
                 data={regiones_comunas.regiones}
+            />
+
+            <ModalConfirmacion
+                show={showConfirmarModal}
+                onClose={() => setShowConfirmarModal(false)}
+                onConfirm={handleConfirmDelete}
+                message={message}
             />
         </>
     )
