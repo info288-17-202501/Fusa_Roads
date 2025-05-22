e868e9506fdf3ddeac2804ca90ddc67d1341089c
handel123 <handel.venegas@alumnos.uach.cl>
2025-05-21 22:25:54 -0400

    avance parcial feature

---
+++ microservicioParametros/app/database/schemas.py
@@ -0,0 +1,13 @@
+from pydantic import BaseModel
+
+class UserCreate(BaseModel):
+    username: str
+    email: str
+
+class User(BaseModel):
+    id: int
+    username: str
+    email: str
+
+    class Config:
+        orm_mode = True
