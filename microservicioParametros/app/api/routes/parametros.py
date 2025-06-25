6b4221e5bc0398a3894ba1911b8defff1387719b
handel123 <handel.venegas@alumnos.uach.cl>
2025-06-25 14:45:58 -0400

    feature mongo

--- microservicioParametros/app/api/routes/parametros.py
+++ microservicioParametros/app/api/routes/parametros.py
@@ -20,50 +20,6 @@ def get_parametro(parametro_id: str):
         raise HTTPException(status_code=400, detail="ID no válido")
 
     if (parametro := conn.fusa.parametrosPia.find_one({"_id": oid})) is not None:
-        #parametro["id"] = str(parametro.pop("_id"))  # <-- Esto quita `_id` y lo asigna a `id`
         return Config.from_mongo(parametro)
 
     raise HTTPException(status_code=404, detail=f"Parametro {parametro_id} no encontrado")
-
-
-# #crear nuevo parametro PIA
-# @router.post("/", response_model=Config, status_code=201)
-# def create_parametro(parametro: Parametro):
-#     parametros = read_json(JSON_PATH)
-    
-#     if any(p["id"] == parametro.id for p in parametros):
-#         raise HTTPException(status_code=400, detail="ID ya existe")
-    
-#     parametros.append(parametro.dict())
-#     write_json(parametros, JSON_PATH)
-#     return parametro
-
-# #actualizar parametro existente PIA
-# @router.put("/{parametro_id}", response_model=Parametro)
-# def update_parametro(parametro_id: int, parametro: Parametro):
-#     parametros = read_json( JSON_PATH)
-#     index = next((i for i, p in enumerate(parametros) if p["id"] == parametro_id), None)
-    
-#     if index is None:
-#         raise HTTPException(status_code=404, detail="Parámetro no encontrado")
-    
-#     if parametro_id != parametro.id:
-#         raise HTTPException(status_code=400, detail="ID no coincide con el cuerpo")
-    
-#     parametros[index] = parametro.dict()
-#     write_json(parametros, JSON_PATH)
-#     return parametro
-
-
-# ## eliminar por id parametro PIA
-# @router.delete("/{parametro_id}", status_code=204)
-# def delete_parametro(parametro_id: int):
-#     parametros = read_json( JSON_PATH)
-#     index = next((i for i, p in enumerate(parametros) if p["id"] == parametro_id), None)
-    
-#     if index is None:
-#         raise HTTPException(status_code=404, detail="Parámetro no encontrado")
-    
-#     parametros.pop(index)
-#     write_json(parametros, JSON_PATH)
-#     return None
